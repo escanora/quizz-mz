55bd1e2b411baca7db0c825cfbb31ab8fdaf7053
Esca <endateberth@gmail.com>
2024-06-28 11:07:44 +0000

    navigation

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@/components/Home.vue';
 import LoginPage from '@/components/Login.vue'
+import TestPage from '@/components/Test.vue'
 
 const routes = [
-  { path: '/', name: 'LoginPage', component: LoginPage },
+  { path: '/', name: 'TestPage', component: TestPage },
   { path: '/accueil', name: 'HomePage', component: HomePage },
+  { path: '/login', name: 'LoginPage', component: LoginPage },
+
+  
 ];
 
 const router = createRouter({
